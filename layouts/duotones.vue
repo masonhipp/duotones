161347842e7bafdc9094d4000ec7b2463616d794
Mason Hipp <mason.hipp@gmail.com>
2018-04-26 07:46:01 -0400

    adding social sharing meta

--- layouts/duotones.vue
+++ layouts/duotones.vue
@@ -7,6 +7,17 @@
 export default {
   components: {
   },
+  head: {
+    meta : [
+      { charset: 'utf-8' },
+      { property: 'og:url', content: 'https://duotones.co' },
+      { property: 'og:title', content: 'Instant Duotone Effect Generator' },
+      { property: 'og:description', content: 'Create beautiful duotone effects instantly in your browser. Dozens of perfect duotone pairings included.' },
+      { property: 'og:image', content: 'https://duotones.co/duotones-preview-4.jpg' },
+      { property: 'og:image:width', content: '1200' },
+      { property: 'og:image:height', content: '630' }
+    ]
+  },
   data () {
     return {
     }
